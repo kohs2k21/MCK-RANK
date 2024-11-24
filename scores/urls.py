9b77c4b020d8e5e611df30b8103c27f8cf55cb8a
illujun <139211273+illujun@users.noreply.github.com>
2024-11-25 02:40:37 +0900

    Delete scores directory

--- scores/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('update/', views.update_score, name='update_score'),
-    path('scores/', views.view_scores, name='view_scores'),
-]
